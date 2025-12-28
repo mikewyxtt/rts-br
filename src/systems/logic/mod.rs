7ff9f31bfdbb6196d4ea6de3650dd3dc8476408d
Mike Wyatt <mikewyxtt@gmail.com>
2025-12-27 19:39:00 -0500

    refactoring

--- src/systems/logic/mod.rs
+++ src/systems/logic/mod.rs
@@ -7,9 +7,7 @@ pub struct LuaVM {
 
 impl LuaVM {
     pub fn new() -> Self {
-        Self {
-            lua: Lua::new(),
-        }
+        Self { lua: Lua::new() }
     }
 }
 
@@ -26,52 +24,49 @@ impl Script {
             started: false,
         }
     }
-}
 
-struct LuaEntityContext {
-    entity: Entity,
-}
-
-impl mlua::UserData for LuaEntityContext {}
-
-pub fn run_logic(
-    time: Res<Time>,
-    lua_vm: NonSend<LuaVM>,
-    mut query: Query<(Entity, &mut Script)>,
-) {
-    let lua = &lua_vm.lua;
-
-    for (entity, mut script) in &mut query {
-
-        // Per-entity environment
-        let env = lua.globals();
-
-        env.set("self", LuaEntityContext { entity }).unwrap();
-
-        lua.load(&script.code).exec().unwrap();
-
-        if !script.started {
-            if let Ok(start) = lua.globals().get::<_, mlua::Function>("Start") {
-                start.call::<_, ()>(()).ok();
-            }
-            script.started = true;
+    /// Initializes the script by calling its Start function.
+    pub fn start(&mut self, lua: &Lua) {
+        lua.load(&self.code).exec().unwrap();
+        if let Ok(start) = lua.globals().get::<_, mlua::Function>("Start") {
+            start.call::<_, ()>(()).ok();
         }
+        self.started = true;
+    }
 
+    /// Calls the Update function of the script. Injects the given delta time.
+    pub fn update(&self, lua: &Lua, delta_time: f32) {
+        lua.load(&self.code).exec().unwrap();
         if let Ok(update) = lua.globals().get::<_, mlua::Function>("Update") {
-            update.call::<_, ()>(time.delta_secs()).ok();
+            update.call::<_, ()>(delta_time).ok();
+        }
+    }
+
+    /// Calls the FixedUpdate function of the script.
+    pub fn fixed_update(&self, lua: &Lua) {
+        lua.load(&self.code).exec().unwrap();
+        if let Ok(fixed_update) = lua.globals().get::<_, mlua::Function>("FixedUpdate") {
+            fixed_update.call::<_, ()>(()).ok();
         }
     }
 }
 
-pub fn run_fixed_logic(
-    lua_vm: NonSend<LuaVM>,
-    mut query: Query<(Entity, &mut Script)>,
-) {
+pub fn run_logic(time: Res<Time>, lua_vm: NonSend<LuaVM>, mut query: Query<(Entity, &mut Script)>) {
     let lua = &lua_vm.lua;
 
-    for (entity, script) in &mut query {
-        if let Ok(fixed_update) = lua.globals().get::<_, mlua::Function>("FixedUpdate") {
-            fixed_update.call::<_, ()>(()).ok();
+    for (_entity, mut script) in &mut query {
+        if !script.started {
+            script.start(lua);
         }
+
+        script.update(lua, time.delta_secs());
+    }
+}
+
+pub fn run_fixed_logic(lua_vm: NonSend<LuaVM>, mut query: Query<(Entity, &mut Script)>) {
+    let lua = &lua_vm.lua;
+
+    for (_entity, script) in &mut query {
+        script.fixed_update(lua);
     }
 }
